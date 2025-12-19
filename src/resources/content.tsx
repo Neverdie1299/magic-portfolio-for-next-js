06f05345347d10ff69d329f886a4cbcd362bd771
NeverDie <zth.mrs@gmail.com>
2025-12-19 12:41:21 +0300

    Contenu de l'entreprise mis à jour2

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -8,7 +8,7 @@ const person: Person = {
   role: "Design Engineer",
   avatar: "/images/avatar.jpg",
   email: "marson.zaiari@gmail.com",
-  location: "Europe/Vienna", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "Indian/Antananarivo", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English"], // optional: Leave the array empty if you don't want to display languages
 };
 
